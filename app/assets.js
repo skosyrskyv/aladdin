4f240af7c31022c92dd2136915a92781089e5115
skosyrskyv <skosyrskyv@gmail.com>
2021-02-13 20:12:13 +0900

    phaser 3 start project

---
+++ app/assets.js
@@ -0,0 +1,47 @@
+
+
+const baseURL = 'src/'
+
+const images = [
+    {
+        name : 'sky',
+        source : 'assets/sky.png'
+    },
+    {
+        name : 'ground',
+        source : 'assets/platform.png'
+    },
+    {
+        name : 'star',
+        source : 'assets/star.png'
+    },
+    {
+        name : 'bomb',
+        source : 'assets/bomb.png'
+    }
+]
+    
+const spritesheet = [
+    {
+        name = 'dude',
+        source = 'assets/dude.png',
+        size = {
+            frameWidth: 32, 
+            frameHeight: 48
+        }
+    }
+]
+
+// const loadAssets = (images, spritesheet, baseURL) =>{
+//     this.load.setBaseURL(baseURL)
+function preload (){
+    images.map(image => {
+        this.load.image(image.name, image.source);
+    })
+
+    spritesheet.map(sprites => {
+        this.load.spritesheet(sprites.name, sprites.source, sprites.size);
+    })
+}
+
+// }
